d1730664e6ecbf033176f1aff0082b44a4371f84
arhangar <arhangar@gmail.com>
2024-01-04 21:40:29 +1100

    added Analyzer decibel marks
    cleaned up code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -237,9 +237,8 @@ void ResponseCurveComponent::paint (juce::Graphics& g)
     g.fillAll(Colours::black);
     
     g.drawImage(background, getLocalBounds().toFloat());
-    
-    //auto responseArea = getLocalBounds();
-    auto responseArea =  getAnalysisArea() ; //getRenderArea();
+
+    auto responseArea =  getAnalysisArea() ; 
     
     
     auto w = responseArea.getWidth() ;
@@ -354,12 +353,12 @@ void ResponseCurveComponent::resized()
     for (auto gDb : gain)
     {
         auto y = jmap(gDb, -24.f, 24.f, float(bottom),float(top));
-//        g.drawHorizontalLine(y, 0, getWidth());
+
         g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::darkgrey );
         g.drawHorizontalLine(y, left, right);
     }
     
-//    g.drawRect(getAnalysisArea());
+
     g.setColour(Colours::lightgrey) ;
     const int fontHeight = 10;
     g.setFont(fontHeight) ;
@@ -402,6 +401,14 @@ void ResponseCurveComponent::resized()
         
         g.setColour(gDb == 0.f ? Colour(6u, 172u, 1u) : Colours::lightgrey );
         g.drawFittedText(str, r, juce::Justification::centred, 1);
+        
+        str.clear();
+        str << (gDb - 24.f) ;
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight) ;
+        g.setColour(Colours::lightgrey);
+        g.drawFittedText(str, r, juce::Justification::centred, 1) ;
     }
 }
 
@@ -409,9 +416,6 @@ void ResponseCurveComponent::resized()
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
 {
     auto bounds = getLocalBounds() ;
-//    bounds.reduce(10,//JUCE_LIVE_CONSTANT(5),
-//                  8 //JUCE_LIVE_CONSTANT(5)
-//                  );
     bounds.removeFromTop(12);
     bounds.removeFromBottom(2);
     bounds.removeFromLeft(20);
